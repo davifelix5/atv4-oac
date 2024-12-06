32f9b24b77f378501aa6f6922f315e2000955619
Davi Felix <davifelix@usp.br>
2024-12-06 02:04:05 -0300

    remove avx512

--- cap4.c
+++ cap4.c
@@ -1,23 +1,21 @@
 #include <x86intrin.h>
 #define UNROLL (4)
 
-void dgemm(int n, double *A, double *B, double *C)
+void dgemm (int n, double* A, double* B, double* C) 
 {
-    for (int i = 0; i < n; i += UNROLL * 8)
-        for (int j = 0; j < n; ++j)
-        {
-            __m512d c[UNROLL];
-            for (int r = 0; r < UNROLL; r++)
-                c[r] = _mm512_load_pd(C + i + r * 8 + j * n); //[ UNROLL];
-
-            for (int k = 0; k < n; k++)
+    for (int i = 0; i < n; i+=UNROLL*4 )
+        for (int j = 0; j < n; j++) { 
+            __m256d c[4];
+            for (int x=0; x < UNROLL; x++)
+                c[x] = _mm256_load_pd(C+i+x*4+j*n);
+            
+            for(int k = 0; k < n; k++ )
             {
-                __m512d bb = _mm512_broadcastsd_pd(_mm_load_sd(B + j * n + k));
-                for (int r = 0; r < UNROLL; r++)
-                    c[r] = _mm512_fmadd_pd(_mm512_load_pd(A + n * k + r * 8 + i), bb, c[r]);
+                __m256d b = _mm256_broadcast_sd (B+k+j*n); 
+                for (int x = 0; x < UNROLL; x++)
+                    c[x] = _mm256_add_pd(c[x], _mm256_mul_pd(_mm256_load_pd(A+n*k+x*4+i), b));
             }
-
-            for (int r = 0; r < UNROLL; r++)
-                _mm512_store_pd(C + i + r * 8 + j * n, c[r]);
+            for (int x=0; x < UNROLL; x++)
+                _mm256_store_pd (C+i+x*4+j*n, c[x]);
         }
 }
--- cap5.c
+++ cap5.c
@@ -1,33 +1,31 @@
-#include <x86intrin.h>
-#define UNROLL (4)
+
+#include <x86intrin.h> 
+#define UNROLL (4) 
 #define BLOCKSIZE 32
 
-void do_block(int n, int si, int sj, int sk,
-              double *A, double *B, double *C)
+void do_block (int n, int si, int sj, int sk, double *A, double *B, double *C)
 {
-    for (int i = si; i < si + BLOCKSIZE; i += UNROLL * 8)
-        for (int j = sj; j < sj + BLOCKSIZE; j++)
-        {
-            __m512d c[UNROLL];
-            for (int r = 0; r < UNROLL; r++)
-                c[r] = _mm512_load_pd(C + i + r * 8 + j * n); //[ UNROLL];
-
-            for (int k = sk; k < sk + BLOCKSIZE; k++)
+    for (int i= si; i < si+BLOCKSIZE; i+=UNROLL*4) 
+        for (int j = sj; j < sj+BLOCKSIZE; j++) { 
+            __m256d c[UNROLL];
+            for (int x=0; x < UNROLL; x++)
+                c[x] = _mm256_load_pd (C + i + x*4 + j*n); /* c[x] = C[i][j] */
+            for(int k = sk; k < sk+BLOCKSIZE; k++ )
             {
-                __m512d bb = _mm512_broadcastsd_pd(_mm_load_sd(B + j * n + k));
-                for (int r = 0; r < UNROLL; r++)
-                    c[r] = _mm512_fmadd_pd(_mm512_load_pd(A + n * k + r * 8 + i), bb, c[r]);
+                __m256d b = _mm256_broadcast_sd(B+k+j*n); /* b B[k][j] */
+                for (int x=0; x < UNROLL; x++)
+                    c[x] = _mm256_add_pd (c[x], /* c[x]+=A[i][k]*b */ 
+                                            _mm256_mul_pd(_mm256_load_pd (A + n*k +x*4 + i), b));
             }
-
-            for (int r = 0; r < UNROLL; r++)
-                _mm512_store_pd(C + i + r * 8 + j * n, c[r]);
+            for (int x=0; x < UNROLL; x++) 
+                _mm256_store_pd (C +i + x*4 + j*n, c[x]); /* C[i][j]=c[x] */
         }
 }
 
-void dgemm(int n, double *A, double *B, double *C)
+void dgemm (int n, double* A, double* B, double* C)
 {
-    for (int sj = 0; sj < n; sj += BLOCKSIZE)
-        for (int si = 0; si < n; si += BLOCKSIZE)
-            for (int sk = 0; sk < n; sk += BLOCKSIZE)
-                do_block(n, si, sj, sk, A, B, C);
+    for (int sj = 0; sj < n; sj += BLOCKSIZE ) 
+        for (int si = 0; si < n; si += BLOCKSIZE ) 
+            for (int sk= 0; sk < n; sk +=BLOCKSIZE)
+                 do_block(n, si, sj, sk, A, B, C);
 }
